3909970910936059c7872bfebe3f19f363fa45f9
Yonghui Chen <chyhfj@gmail.com>
2015-03-31 00:14:06 +0800

    support x86

---
+++ toolchain_build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+checkError()
+{
+    if [ "${1}" -ne "0" ]; then
+        echo "*** Error: ${2}"
+        exit ${1}
+    fi
+}
+
+if [ ! -f $MIRAI_SDK_PREFIX/lib/libMediaPlayer.so ]; then
+	pushd $MIRAI_PROJECT_ROOT_PATH/Mirai-MediaPlayer
+	xcodebuild -target MediaPlayer-Android -xcconfig xcconfig/Android-$ABI.xcconfig clean
+	
+	xcodebuild -target MediaPlayer-Android -xcconfig xcconfig/Android-$ABI.xcconfig
+	checkError $? "build MediaPlayer failed"
+	
+	#clean up
+	rm -r build
+	popd
+fi
